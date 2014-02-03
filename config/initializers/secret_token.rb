5a54e321911d0d3c807df8b30beb7f9765a98895
skellz <sherwin.kelly@gmail.com>
2014-02-03 15:22:59 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Groupize::Application.config.secret_key_base = '0b2e25dcfa44723e4a67507f58dbd7c568fd2b0b323df7b656e593482b67cb7812285f02bae07ba5c460c0e97f4b847b72f1bab9251732ef4f2c43f750d19db4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Groupize::Application.config.session_store :cookie_store, key: '_groupize_session'
