f0eb7658d66aea4ce6a80543e4eb39d59af55d13
skellz <sherwin.kelly@gmail.com>
2014-02-25 10:51:48 -0500

    added select file notice

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -2,12 +2,18 @@ class UploadsController < ApplicationController
   def index
   end
   def uploadFile
-    file = File.open(DataFile.save(params[:upload]))
-    doc = Nokogiri::XML::Document.parse(file)
-    file.close
-    feed = DataFile.new(doc)
-    feed.feed_data
-    
-    redirect_to root_path
+    binding.pry
+    if params[:upload] == nil
+      flash[:notice] = "Please select a file"
+      redirect_to uploads_path
+    else
+      file = File.open(DataFile.save(params[:upload]))
+      doc = Nokogiri::XML::Document.parse(file)
+      file.close
+      feed = DataFile.new(doc)
+      feed.feed_data
+      
+      redirect_to root_path
+    end
   end
 end
--- app/models/data_file.rb
+++ app/models/data_file.rb
@@ -18,8 +18,8 @@ class DataFile < ActiveRecord::Base
       scene_obj = Scene.find_or_create_by(title: title)
       scene.search('SPEECH').each do |speech|
         speaker = speech.search('SPEAKER').text
-        speaker_obj = Speaker.find_or_create_by( name: speaker )
-        speech_obj = scene_obj.speeches.create( speaker_id: speaker_obj.id )
+        speaker_obj = Speaker.find_or_create_by(name: speaker)
+        speech_obj = scene_obj.speeches.create(speaker_id: speaker_obj.id)
         speech.search('LINE').each do |l|
           Line.create(speech_id: speech_obj.id, line: l.text)
         end
